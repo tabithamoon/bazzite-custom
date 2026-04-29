6e803d2119bfb65654e5ba6ec914a9c77023f28f
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2026-04-29 15:38:33 -0300

    Remove KDE package removals [no ci]

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,9 +8,6 @@ dnf5 -y copr enable ublue-os/packages
 # Install general packages
 dnf5 -y install btop fish
 
-# Remove packages
-dnf5 -y remove kate kcm-fcitx5 fcitx5-*
-
 # Disable COPRs
 dnf5 -y copr disable ublue-os/packages
 
