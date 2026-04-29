277be05c862f36efbab9a1857b1ccb47a0b1871a
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2026-04-29 16:02:21 -0300

    Add zenergy akmod

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,14 +4,19 @@ set -ouex pipefail
 ### Packages
 # Enable COPRs
 dnf5 -y copr enable ublue-os/packages
+dnf5 -y copr enable ublue-os/akmods
 
 # Install general packages
 dnf5 -y install btop fish
+
+# Install akmods
+dnf5 -y install akmod-zenergy
 
 # Remove unneeded packages
 dnf5 -y remove gnome-software* firefox*
 
 # Disable COPRs
+dnf5 -y copr disable ublue-os/akmods
 dnf5 -y copr disable ublue-os/packages
 
 ### System units
