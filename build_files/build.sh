c9fff8e4638e340ea719d0e0d714acb298bb8caa
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2026-07-29 14:13:18 +0000

    Remove more packages, add Faugus Launcher

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,16 +3,23 @@ set -ouex pipefail
 
 ### Packages
 # Enable COPRs
+dnf5 -y copr enable faugus/faugus-launcher
 dnf5 -y copr enable ublue-os/packages
 
 # Install general packages
 dnf5 -y install btop android-tools
 
+# Install gaming related packages
+dnf5 -y install faugus-launcher
+
 # Remove unused packages
-dnf5 -y remove kate kde-connect* kcm-fcitx5 fcitx5-* 
+dnf5 -y remove kcm-fcitx5 fcitx5-* kate \
+    kdeconnectd kde-connect* \
+    lutris tailscale 
 
 # Disable COPRs
 dnf5 -y copr disable ublue-os/packages
+dnf5 -y copr disable faugus/faugus-launcher
 
 ### System units
 # systemctl enable podman.socket
