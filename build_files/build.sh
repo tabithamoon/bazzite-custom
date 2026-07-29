52c33aaf1918a87c203c666752a900f7ef89a685
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2026-07-29 13:59:15 +0000

    Re-add add and remove packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,9 +3,16 @@ set -ouex pipefail
 
 ### Packages
 # Enable COPRs
-#dnf5 -y copr enable ublue-os/packages
+dnf5 -y copr enable ublue-os/packages
 
-#dnf5 -y copr disable ublue-os/packages
+# Install general packages
+dnf5 -y install btop android-tools
+
+# Remove unused packages
+dnf5 -y remove kate kde-connect* kcm-fcitx5 fcitx5-* 
+
+# Disable COPRs
+dnf5 -y copr disable ublue-os/packages
 
 ### System units
 # systemctl enable podman.socket
